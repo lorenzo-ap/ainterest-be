e6b341f8ab1f3bd8a19bdf3b4a31f0507731efdc
APOLLOL <lorenzo.apolloni@globallogic.com>
2024-07-19 14:51:48 +0300

    removed image-geneartion API call

--- api/index.js
+++ api/index.js
@@ -4,7 +4,6 @@ import cors from 'cors';
 
 import connectDB from '../mongodb/connect.js';
 import postRoutes from '../routes/postRoutes.js';
-import imageGeneratorRoutes from '../routes/imageGeneratorRoutes.js';
 
 dotenv.config();
 
@@ -14,7 +13,6 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);
-app.use('/api/v1/image-generator', imageGeneratorRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello !');
