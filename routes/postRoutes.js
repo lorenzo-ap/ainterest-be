e831440993f8f4a4cf5a36d9991f4d484982877a
lorenzo-ap <ap.lorenzo03@gmail.com>
2025-01-02 19:37:03 +0200

    fix: vercel env vars

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,7 +1,6 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
 import { v2 as cloudinary } from 'cloudinary';
-
+import * as dotenv from 'dotenv';
+import express from 'express';
 import Post from '../mongodb/models/post.js';
 
 dotenv.config();
