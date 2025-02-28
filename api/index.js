2686d2e136cf8d21aa8abdc9836e62ce32e842dd
lorenzo-ap <ap.lorenzo03@gmail.com>
2025-02-28 15:46:49 +0200

    refactor: users -> user route

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/posts', postRoutes);
 
 app.get('/', (req, res) => {
