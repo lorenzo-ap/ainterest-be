e3976c1407aece97cc60aea807a7e3f76b379402
lorenzo-ap <ap.lorenzo03@gmail.com>
2025-06-03 15:58:03 +0300

    feat: generate-image endpoint

--- controllers/postController.js
+++ controllers/postController.js
@@ -114,4 +114,45 @@ const likePost = async (req, res) => {
 	}
 };
 
-export { createPost, deletePost, getPosts, getUserPosts, likePost };
+/**
+ * @desc Generate an image based on the provided prompt and dimensions
+ * @route POST /api/v1/posts/generate-image
+ * @access Private
+ */
+const generateImage = async (req, res) => {
+	try {
+		const { prompt, size } = req.body;
+
+		if (!prompt) {
+			return res.status(400).json({ error: 'Prompt is required' });
+		}
+
+		const response = await fetch(
+			`https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/ai/run/@cf/stabilityai/stable-diffusion-xl-base-1.0`,
+			{
+				method: 'POST',
+				headers: {
+					Authorization: `Bearer ${process.env.CLOUDFLARE_API_TOKEN}`,
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ prompt, negative_prompt: 'NSFW', width: size, height: size }),
+			}
+		);
+
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
+
+		const imageBuffer = await response.arrayBuffer();
+		const base64Image = Buffer.from(imageBuffer).toString('base64');
+
+		res.status(200).json({
+			image: `data:image/png;base64,${base64Image}`,
+		});
+	} catch (error) {
+		console.error('Error:', error.message);
+		res.status(500).json({ error: 'Internal server error' });
+	}
+};
+
+export { createPost, deletePost, generateImage, getPosts, getUserPosts, likePost };
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,5 +1,12 @@
 import express from 'express';
-import { createPost, deletePost, getPosts, getUserPosts, likePost } from '../controllers/postController.js';
+import {
+	createPost,
+	deletePost,
+	generateImage,
+	getPosts,
+	getUserPosts,
+	likePost,
+} from '../controllers/postController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
@@ -10,5 +17,7 @@ router.get('/', getPosts);
 router.get('/:id', getUserPosts);
 router.put('/:id', protect, likePost);
 router.delete('/:id', protect, deletePost);
+
+router.post('/generate-image', protect, generateImage);
 
 export default router;
